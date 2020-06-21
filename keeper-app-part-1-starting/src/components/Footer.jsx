c4c4b78d39aaf67ceede114bec73917da0757137
parth <itsparth20@gmail.com>
2020-06-20 17:38:59 -0700

    Keeper App

---
+++ keeper-app-part-1-starting/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const today = new Date()
+const year = today.getFullYear()
+
+function Footer() {
+    return <footer > <p> Copyright© { year } </p></footer> ;
+}
+
+export default Footer;
